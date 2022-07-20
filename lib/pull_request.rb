1ba80b588327e5c97a2af5ce241c47b0bdd6c41f
Cristina <579522+CristinaRO@users.noreply.github.com>
2022-07-20 18:56:03 +0100

    PullRequest does data transformation for Influx
    Move the method into the PullRequest class, which now has all the data
    required to combine and transform the PR data with the release data in
    the shape we want to store in InfluxDB.

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -1,9 +1,11 @@
 class PullRequest
   attr_accessor :number, :started_time, :opened_time, :merged_time,
     :number_of_commits, :total_line_changes, :number_of_reviews, :number_of_comments
+  attr_reader :release
 
-  def initialize(number)
+  def initialize(number, release:)
     @number = number
+    @release = release
   end
 
   def ==(other)
@@ -17,4 +19,55 @@ class PullRequest
       number_of_comments == other.number_of_comments
   end
   alias_method :eql?, :==
+
+  ### GENERIC FIELDS
+  #
+  # The timestamp, _time, is what Influx will interpret as the time the "measured event" happened,
+  #   and it must be a Unix timestamp
+  # The range is mandatory for all, or almost all, Flux queries, and it refers to the time window
+  #   for which to process measurement data
+  #   _start and _stop are special Influx fields that map to the start and end of this time window
+  # The special _measurement field corresponds to the "name", and it is how we can filter
+  #   for the entire stream of these specific "measurements"
+  # Tags will allow us to filter and group projects, environments, and individual deploys
+  #
+  ### PULL REQUEST TAGS AND FIELDS
+  #
+  # Pull request data is meant to serve our goals of measuring how long it takes our work
+  #   to go from start to being available to users on production, and identifying where there are any
+  #   slow points in the development pipeline
+  #
+  # Time-related measurements
+  #   * the oldest commit in the PR is a proxy for when work on a specific story started
+  #   * when PRs are opened / merged, to measure whether there are any bottlenecks in our review processes
+  #   * the time when the work was deployed
+  #
+  # Code-related measurements
+  #   * total number of commits in a PR
+  #   * total line changes in a PR
+  #   * average number of line changes per commit in a PR
+  #   * number of reviews on a PR
+  #   * number of comments on a PR
+  def data_for_influx
+    {
+      name: "deployment",
+      tags: {
+        project: release.project,
+        env: release.env,
+        pr: number.to_s,
+        deploy_sha: release.head_sha
+      },
+      fields: {
+        seconds_since_first_commit: release.deploy_time.to_i - started_time.to_i,
+        seconds_since_pr_opened: release.deploy_time.to_i - opened_time.to_i,
+        seconds_since_pr_merged: release.deploy_time.to_i - merged_time.to_i,
+        number_of_commits_in_pr: number_of_commits,
+        total_line_changes_in_pr: total_line_changes,
+        average_changes_per_commit_in_pr: (total_line_changes / number_of_commits),
+        number_of_reviews_on_pr: number_of_reviews,
+        number_of_comments_on_pr: number_of_comments
+      },
+      time: release.deploy_time.to_i
+    }
+  end
 end
--- lib/release_analyser.rb
+++ lib/release_analyser.rb
@@ -12,55 +12,8 @@ class ReleaseAnalyser
     self.release = release
   end
 
-  ### GENERIC FIELDS
-  #
-  # The timestamp, _time, is what Influx will interpret as the time the "measured event" happened,
-  #   and it must be a Unix timestamp
-  # The range is mandatory for all, or almost all, Flux queries, and it refers to the time window
-  #   for which to process measurement data
-  #   _start and _stop are special Influx fields that map to the start and end of this time window
-  # The special _measurement field corresponds to the "name", and it is how we can filter
-  #   for the entire stream of these specific "measurements"
-  # Tags will allow us to filter and group projects, environments, and individual deploys
-  #
-  ### PULL REQUEST TAGS AND FIELDS
-  #
-  # Pull request data is meant to serve our goals of measuring how long it takes our work
-  #   to go from start to being available to users on production, and identifying where there are any
-  #   slow points in the development pipeline
-  #
-  # Time-related measurements
-  #   * the oldest commit in the PR is a proxy for when work on a specific story started
-  #   * when PRs are opened / merged, to measure whether there are any bottlenecks in our review processes
-  #   * the time when the work was deployed
-  #
-  # Code-related measurements
-  #   * total number of commits in a PR
-  #   * total line changes in a PR
-  #   * average number of line changes per commit in a PR
-  #   * number of reviews on a PR
-  #   * number of comments on a PR
-  def pr_data_for_influx(pr)
-    {
-      name: "deployment",
-      tags: {
-        project: release.project,
-        env: release.env,
-        pr: pr.number.to_s,
-        deploy_sha: release.head_sha
-      },
-      fields: {
-        seconds_since_first_commit: release.deploy_time.to_i - pr.started_time.to_i,
-        seconds_since_pr_opened: release.deploy_time.to_i - pr.opened_time.to_i,
-        seconds_since_pr_merged: release.deploy_time.to_i - pr.merged_time.to_i,
-        number_of_commits_in_pr: pr.number_of_commits,
-        total_line_changes_in_pr: pr.total_line_changes,
-        average_changes_per_commit_in_pr: (pr.total_line_changes / pr.number_of_commits),
-        number_of_reviews_on_pr: pr.number_of_reviews,
-        number_of_comments_on_pr: pr.number_of_comments
-      },
-      time: release.deploy_time.to_i
-    }
+  def pull_requests_data_for_influx
+    get_pull_requests.map(&:data_for_influx)
   end
 
   # A release is delimited by the head_sha (the commit at the HEAD of the branch at the time of deploy),
@@ -69,12 +22,6 @@ class ReleaseAnalyser
   #
   # Analysing the release means identifying which pull requests contributed to it,
   # and then collecting all the data we want to store in InfluxDB for each PR
-  def pull_requests_data_for_influx
-    get_pull_requests.map do |pr|
-      pr_data_for_influx(pr)
-    end
-  end
-
   def get_pull_requests
     repo = release.repo
     commits_between = git_client.compare(repo, release.starting_sha, release.head_sha).commits
@@ -90,7 +37,7 @@ class ReleaseAnalyser
 
       pr_number = pull_request.number
       if (pr = pull_requests.find { |pr| pr.number == pr_number }).nil?
-        pr = PullRequest.new(pr_number)
+        pr = PullRequest.new(pr_number, release: release)
 
         pr.started_time = authored_date(commit)
         pr.opened_time = pull_request.created_at
