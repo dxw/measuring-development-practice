f4bbb85cec4b0ac58027721f19ed43d0b3c73998
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-03-17 17:14:46 +0000

    WIP List repositories without teams
    Repositories that don't give access to the tech team raise an error
    when the dxw-rails-user tries to inspect their teams, so rather than
    being able to list explicitly which repositories don't give access to
    which teams, we have to rely on the proxy indicator of listing repos for
    which the teams couldn't be accessed.
    
    This branch should be its own repo, but it was easier to kickstart the
    spike from an existing one.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Measuring development practice
 
 Please see the [wiki](https://github.com/dxw/measuring-development-practice/wiki)
+
+## Listing repositories without teams
+
+```
+bundle exec ruby scripts/repos.rb
+```
---
+++ scripts/repos.rb
@@ -0,0 +1,69 @@
+require "net/http"
+require "octokit"
+require "pp"
+
+require "dotenv"
+Dotenv.load
+
+def topics(repo_name)
+  topics = @git_client.topics(repo_name, {accept: Octokit::Preview::PREVIEW_TYPES[:topics]})
+  return [] if topics.nil? || topics == {}
+
+  topics[:names]
+end
+
+@git_client = Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
+
+page = 1
+repos = []
+
+# repos = @git_client.organization_repositories("dxw", per_page: 100, page: page).reject(&:archived?)
+
+while true
+  puts "Fetching page #{page}..."
+  page_repos = @git_client.organization_repositories("dxw", per_page: 100, page: page)
+  break if page_repos.empty?
+
+  repos += page_repos.reject(&:archived?)
+  page +=1
+end
+
+repos_without_tech_team = []
+private_repos_without_staff_team = []
+repos_unknown_teams = []
+
+repos.each do |repo|
+  print "."
+
+  begin
+    teams = @git_client.teams(repo.full_name).map(&:slug)
+
+    if !teams.include?("technology-team") && !topics(repo.full_name).include?("govpress")
+      repos_without_tech_team << repo.full_name
+    end
+
+    if repo.private? && !teams.include?("staff") && !topics(repo.full_name).include?("govpress")
+      private_repos_without_staff_team << repo.full_name
+    end
+  rescue Exception
+    repos_unknown_teams << repo.full_name
+  end
+end
+
+unless repos_without_tech_team.empty?
+  puts "The following repositories do not give access to the 'technology-team' team:"
+  puts "\t#{repos_without_tech_team.join("\n\t")}"
+  puts "Contact the repository owners or a dxw GitHub admin if you think the tech team should have access."
+end
+
+unless private_repos_without_staff_team.empty?
+  puts "The following private repositories do not give access to the 'staff' team:"
+  puts "\t#{private_repos_without_staff_team.join("\n\t")}"
+  puts "Contact the repository owners or a dxw GitHub admin if you think the staff team should have access."
+end
+
+unless repos_unknown_teams.empty?
+  puts "The script could not determine which teams have access to the following repositories:"
+  puts "\t#{repos_unknown_teams.join("\n\t")}"
+  puts "Contact the repository owners or a dxw GitHub admin if you think the tech and/or staff teams should have access."
+end
